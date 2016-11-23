c0cb23faa6543963bbf865eece95923ebdfdd7f5
Dikesh-Chokshi <dikesh@cfl.rr.com>
2016-11-22 21:50:46 -0500

    Handle ETF portfolio

--- GetStockStats/Models/Stats.cs
+++ GetStockStats/Models/Stats.cs
@@ -9,104 +9,118 @@ using System.Text;
 
 namespace GetStockStats.Models
 {
-    [DataContract]
-    class Stats : BaseRepository
+  enum EQUITY_TYPE {
+    STOCKS=1,
+    ETF=2,
+    MUTUAL_FUND=3
+  }
+
+  [DataContract]
+  class Stats : BaseRepository
+  {
+    [DataMember]
+    public EQUITY_TYPE equity_type { get; set; }
+
+    [DataMember]
+    public long db_ticker_id { get; set; }
+
+    [DataMember]
+    public DateTime db_updated { get; set; }
+
+    [DataMember]
+    public decimal? db_revenue { get; set; }
+
+    [DataMember]
+    public decimal? db_net_income { get; set; }
+
+    [DataMember]
+    public long? db_share_outstanding { get; set; }
+
+    [DataMember]
+    public decimal? db_current_price { get; set; }
+
+    [DataMember]
+    public decimal? db_ebitda { get; set; }
+
+    [DataMember]
+    public decimal? db_MA50 { get; set; }
+
+    [DataMember]
+    public decimal? db_MA200 { get; set; }
+
+    [DataMember]
+    public decimal? db_PEGRatio { get; set; }
+
+    [DataMember]
+    public decimal? db_DividendYield { get; set; }
+
+    public List<Stats> Get(string sql)
     {
-        [DataMember]
-        public long db_ticker_id { get; set; }
+      //SQL DB Access
+      using (IDbConnection connection = OpenConnection("StockDB"))
+      {
+        StringBuilder query = new StringBuilder();
+        // Dapper maps by column names. So create class with identical member names as the columns in select
+        query.Append(sql);
 
-        [DataMember]
-        public DateTime db_updated { get; set; }
-
-        [DataMember]
-        public decimal? db_revenue { get; set; }
-
-        [DataMember]
-        public decimal? db_net_income { get; set; }
-
-        [DataMember]
-        public long? db_share_outstanding { get; set; }
-
-        [DataMember]
-        public decimal? db_current_price { get; set; }
-
-        [DataMember]
-        public decimal? db_ebitda { get; set; }
-
-        [DataMember]
-        public decimal? db_MA50 { get; set; }
-
-        [DataMember]
-        public decimal? db_MA200 { get; set; }
-
-        [DataMember]
-        public decimal? db_PEGRatio { get; set; }
-
-
-        public List<Stats> Get(string sql)
-        {
-            //SQL DB Access
-            using (IDbConnection connection = OpenConnection("StockDB"))
-            {
-                StringBuilder query = new StringBuilder();
-                // Dapper maps by column names. So create class with identical member names as the columns in select
-                query.Append(sql);
-
-                //IEnumerable<Tickers> tickers =  connection.Query<Tickers>(query);
-                var stats = connection.QueryMultiple(query.ToString());
-                var Zstats = stats.Read<Stats>().ToList();
-                return Zstats;
-            }
-        }
-
-        public void Insert(Stats sData)
-        {
-            if (sData.db_net_income <= 0) return;
-            if (sData.db_revenue <= 0) return;
-            if (sData.db_ebitda <= 0) return;
-            if (sData.db_share_outstanding <= 0) return;
-
-            //SQL DB Access
-            string query = "";
-            using (IDbConnection connection = OpenConnection("StockDB"))
-            {
-                query = "INSERT INTO tbl_Stats(db_ticker_id, db_revenue, db_net_income, db_share_outstanding, db_current_price, db_ebitda, db_MA50, db_MA200, db_PEGRatio) " +
-                      "VALUES (@db_ticker_id, @db_revenue, @db_net_income,  @db_share_outstanding, @db_current_price, @db_ebitda, @db_MA50, @db_MA200, @db_PEGRatio)";
-
-                connection.Execute(query, sData);
-            }
-
-        }
-        public int Update(Stats sData)
-        {
-            if (sData.db_net_income <= 0 || sData.db_revenue <= 0 || sData.db_share_outstanding <= 0 || sData.db_ebitda <= 0) {
-                // delete this ticker from Stats
-                Delete(sData);
-                return 0;
-            }
-            //SQL DB Access
-            string query = "";
-            using (IDbConnection connection = OpenConnection("StockDB"))
-            {
-                query = "Update tbl_Stats set db_revenue=@db_revenue, db_updated=@db_updated, db_net_income=@db_net_income, db_share_outstanding=@db_share_outstanding, db_current_price=@db_current_price, db_ebitda=@db_ebitda, db_MA50=@db_MA50, db_MA200=@db_MA200, db_PEGRatio=@db_PEGRatio where db_ticker_id = @db_ticker_id ";
-
-                var count = connection.Execute(query, sData);
-                return count;
-
-            }
-        }
-        public int Delete(Stats sData)
-        {
-            //SQL DB Access
-            string query = "";
-            using (IDbConnection connection = OpenConnection("StockDB"))
-            {
-                query = "delete from tbl_Stats where db_ticker_id = @db_ticker_id ";
-
-                var count = connection.Execute(query, sData);
-                return count;
-
-            }
-        }
+        //IEnumerable<Tickers> tickers =  connection.Query<Tickers>(query);
+        var stats = connection.QueryMultiple(query.ToString());
+        var Zstats = stats.Read<Stats>().ToList();
+        return Zstats;
+      }
     }
+
+    public void Insert(Stats sData)
+    {
+      if (sData.equity_type == EQUITY_TYPE.STOCKS)
+      {
+        if (sData.db_net_income <= 0) return;
+        if (sData.db_revenue <= 0) return;
+        if (sData.db_ebitda <= 0) return;
+        if (sData.db_share_outstanding <= 0) return;
+      }
+      //SQL DB Access
+      string query = "";
+      using (IDbConnection connection = OpenConnection("StockDB"))
+      {
+        query = "INSERT INTO tbl_Stats(db_ticker_id, db_revenue, db_net_income, db_share_outstanding, db_current_price, db_ebitda, db_MA50, db_MA200, db_PEGRatio,db_DividendYield) " +
+              "VALUES (@db_ticker_id, @db_revenue, @db_net_income,  @db_share_outstanding, @db_current_price, @db_ebitda, @db_MA50, @db_MA200, @db_PEGRatio,@db_DividendYield)";
+
+        connection.Execute(query, sData);
+      }
+
+    }
+    public int Update(Stats sData)
+    {
+      if ( sData.equity_type == EQUITY_TYPE.STOCKS & (sData.db_net_income <= 0 || sData.db_revenue <= 0 || sData.db_share_outstanding <= 0 || sData.db_ebitda <= 0))
+      {
+        // delete this ticker from Stats
+        Delete(sData);
+        return 0;
+      }
+      //SQL DB Access
+      string query = "";
+      using (IDbConnection connection = OpenConnection("StockDB"))
+      {
+        query = "Update tbl_Stats set db_revenue=@db_revenue, db_updated=@db_updated, db_net_income=@db_net_income, db_share_outstanding=@db_share_outstanding, db_current_price=@db_current_price, db_ebitda=@db_ebitda, db_MA50=@db_MA50, db_MA200=@db_MA200, db_PEGRatio=@db_PEGRatio, db_DividendYield=@db_DividendYield where db_ticker_id = @db_ticker_id ";
+
+        var count = connection.Execute(query, sData);
+        return count;
+
+      }
+    }
+    public int Delete(Stats sData)
+    {
+      //SQL DB Access
+      string query = "";
+      using (IDbConnection connection = OpenConnection("StockDB"))
+      {
+        query = "delete from tbl_Stats where db_ticker_id = @db_ticker_id ";
+
+        var count = connection.Execute(query, sData);
+        return count;
+
+      }
+    }
+  }
 }
--- GetStockStats/Program.cs
+++ GetStockStats/Program.cs
@@ -6,106 +6,105 @@ using System.Text.RegularExpressions;
 
 namespace GetStockStats
 {
-    class Program : BaseRepository
+  class Program : BaseRepository
+  {
+    static void Main(string[] args)
     {
+      // looking for args like P=IVV T=AAPL
+      string pattern = @"^(\w+)[=](\w+)$";
 
-  
+      EQUITY_TYPE et;
 
-        static void Main(string[] args)
+      // default sql in case no args are provided
+      string sql = "select * from tbl_Portfolios where db_portfolio_name = 'IVV' order by db_strTicker";
+      foreach (var expression in args)
+        foreach (Match m in Regex.Matches(expression, pattern))
         {
-            // looking for args like P=IVV T=AAPL
-            string pattern = @"^(\w+)[=](\w+)$";
+          string key = m.Groups[1].Value;
+          string value = m.Groups[2].Value;
+          Console.WriteLine("{0}  {1}", key, value);
+          switch (key.ToUpper())
+          {
+            case "P":
+              sql = string.Format("select P.*, T.db_type from tbl_Portfolios P, tbl_Ticker T where P.db_portfolio_name = '{0}' and P.db_ticker_id = T.db_ticker_id order by db_strTicker", value);
+              Console.WriteLine(sql);
+              break;
+            case "T":
+              sql = string.Format("select * from tbl_Ticker where db_strTicker = '{0}'", value);
+              Console.WriteLine(sql);
+              break;
 
-            // default sql in case no args are provided
-            string sql = "select* from tbl_Portfolios where db_portfolio_name = 'IVV' order by db_strTicker";
-            foreach (var expression in args)
-                foreach (Match m in Regex.Matches(expression, pattern))
-                {
-                    string key = m.Groups[1].Value;
-                    string value = m.Groups[2].Value;
-                    Console.WriteLine("{0}  {1}", key, value);
-                    switch ( key.ToUpper())
-                    {
-                        case "P":
-                            sql = string.Format("select * from tbl_Portfolios where db_portfolio_name = '{0}'  order by db_strTicker", value);
-                            Console.WriteLine(sql);
-                            break;
-                        case "T":
-                            sql = string.Format("select * from tbl_Ticker where db_strTicker = '{0}'", value);
-                            Console.WriteLine(sql);
-                            break;
+            default:
+              //Console.WriteLine("invalid arg");
+              break;
+          }
+        }
+      YQuoteAPI yqAPI = new YQuoteAPI();
+      YahooQuote yq;
 
-                        default:
-                            //Console.WriteLine("invalid arg");
-                            break;
-                    }
-                }
-            YQuoteAPI yqAPI = new YQuoteAPI();
-            YahooQuote yq;
+      YStatsAPI ysAPI = new YStatsAPI();
+      YStats ys = null;
 
-            YStatsAPI ysAPI = new YStatsAPI();
+      List<Tickers> tickers;
+      Tickers clsTicker = new Tickers();
+      tickers = clsTicker.Get(sql);
 
-            YStats ys;
-            List<Tickers> tickers;
-            Tickers clsTicker = new Tickers();
-            tickers = clsTicker.Get(sql);
-
-            Stats clsStats = new Stats();
-            List<Stats> stats = clsStats.Get("SELECT * from tbl_Stats");
+      Stats clsStats = new Stats();
+      List<Stats> stats = clsStats.Get("SELECT * from tbl_Stats");
 
 
-            for (int i = 0; i < tickers.Count(); i++)
-            {
-                ys = ysAPI.GetData(tickers.ElementAt(i).db_strTicker);
-                yq = yqAPI.GetData(tickers.ElementAt(i).db_strTicker);
-                Stats s = new Stats();
-                s.db_updated = DateTime.Today;
-                try
-                {
-                    s.db_ticker_id = tickers.ElementAt(i).db_ticker_id;
-                    s.db_net_income = ys.quoteSummary.result[0].defaultKeyStatistics.netIncomeToCommon.raw;
-                    s.db_ebitda = ys.quoteSummary.result[0].financialData.ebitda.raw;
-                    s.db_revenue = ys.quoteSummary.result[0].financialData.totalRevenue.raw;
-                    s.db_share_outstanding = ys.quoteSummary.result[0].defaultKeyStatistics.sharesOutstanding.raw;
-                    s.db_current_price = System.Convert.ToDecimal(ys.quoteSummary.result[0].financialData.currentPrice.raw);
-                    s.db_MA50 = System.Convert.ToDecimal(yq.quote.FiftydayMovingAverage);
-                    s.db_MA200 = System.Convert.ToDecimal(yq.quote.TwoHundreddayMovingAverage);
-                    s.db_PEGRatio = System.Convert.ToDecimal(yq.quote.PEGRatio);
+      for (int i = 0; i < tickers.Count(); i++)
+      {
+        Console.WriteLine("Processing {0}", tickers.ElementAt(i).db_strTicker);
+        ys = ysAPI.GetData(tickers.ElementAt(i).db_strTicker);
+        yq = yqAPI.GetData(tickers.ElementAt(i).db_strTicker);
+        Stats s = new Stats();
+        s.db_updated = DateTime.Today;
+        try
+        {
+          s.equity_type = (EQUITY_TYPE)Enum.ToObject(typeof(EQUITY_TYPE), tickers.ElementAt(i).db_type);
+          s.db_ticker_id = tickers.ElementAt(i).db_ticker_id;
+          s.db_current_price = Convert.ToDecimal(yq.quote.LastTradePriceOnly);
+          s.db_MA50 = Convert.ToDecimal(yq.quote.FiftydayMovingAverage);
+          s.db_MA200 = Convert.ToDecimal(yq.quote.TwoHundreddayMovingAverage);
 
-                }
-                catch (Exception e)
-                {
-                    continue;
-                }
+          if (tickers.ElementAt(i).db_type == Convert.ToInt16(EQUITY_TYPE.STOCKS))
+          {
+            s.db_net_income = ys.quoteSummary.result[0].defaultKeyStatistics.netIncomeToCommon.raw;
+            s.db_ebitda = ys.quoteSummary.result[0].financialData.ebitda.raw;
+            s.db_revenue = ys.quoteSummary.result[0].financialData.totalRevenue.raw;
+            s.db_share_outstanding = ys.quoteSummary.result[0].defaultKeyStatistics.sharesOutstanding.raw;
+            s.db_PEGRatio = Convert.ToDecimal(yq.quote.PEGRatio);
+            s.db_DividendYield = Convert.ToDecimal(yq.quote.DividendYield);
+          }
 
-                if (stats.Exists(x => x.db_ticker_id == tickers.ElementAt(i).db_ticker_id)) {
-                    // update
-                    try
-                    {
-                        clsStats.Update(s);
-                    }
-                    catch (Exception e) { }
-                }
-                else
-                {
-                    // insert
-                    try
-                    {
-                        clsStats.Insert(s);
-                    }
-                    catch (Exception e) { }
-                }
-            }
-            Console.WriteLine("Press any key to continue..");
-            Console.ReadKey();
+        }
+        catch (Exception e)
+        {
+          continue;
         }
 
-
-        static void doCalc(long ni, long rev, long shares)
+        if (stats.Exists(x => x.db_ticker_id == tickers.ElementAt(i).db_ticker_id))
         {
-            Console.WriteLine("NI: {0}", ni);
-            Console.WriteLine("Revenue: {0}", rev);
-            Console.WriteLine("Shares: {0}", shares);
+          // update
+          try
+          {
+            clsStats.Update(s);
+          }
+          catch (Exception e) { }
         }
+        else
+        {
+          // insert
+          try
+          {
+            clsStats.Insert(s);
+          }
+          catch (Exception e) { }
+        }
+      }
+      Console.WriteLine("Press any key to continue..");
+      Console.ReadKey();
     }
+  }
 }
